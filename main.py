92d8ae7e4ea9ac288e3f8e470eba5d229990d1d3
Harrison Kim <wootak95@gmail.com>
2023-02-28 18:42:28 +0900

    Feb 28 	modified:   main.py

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
+from typing import Optional
 from fastapi import FastAPI
+from fastapi.params import Body
+from pydantic import BaseModel
+
 
 app = FastAPI()
 
+class Post(BaseModel):
+    title: str
+    content: str
+    published: bool = True
+    rating: Optional[int] = None
 
 @app.get("/") 
 async def root():
@@ -10,3 +19,9 @@ async def root():
 @app.get("/posts")
 def get_posts():
     return {"data": "This is your posts"}
+
+@app.post("/createposts")
+def create_posts(post: Post):
+    print(post)
+    print(post.dict())
+    return {"data": post}
