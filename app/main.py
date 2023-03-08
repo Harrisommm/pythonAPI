07e9e12756549013dd85394ea36730e19acfb2a7
Harrison Kim <wootak95@gmail.com>
2023-03-09 01:42:32 +0900

    response model -no password passed 	modified:   app/main.py

--- app/main.py
+++ app/main.py
@@ -109,3 +109,10 @@ def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
     db.commit()
     db.refresh(new_user)
     return new_user
+
+@app.get('/users/{id}', response_model = schemas.UserOut)
+def get_user(id: int, db: Session = Depends(get_db)):
+    user = db.query(models.User).filter(models.User.id == id).first()
+    if not user:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail = f"User with id:{id} was not found")
+    return user
