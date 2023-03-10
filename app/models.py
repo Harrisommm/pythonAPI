d3d3e1b1b88047a27c5aad130b53bf34e1683530
Harrison Kim <wootak95@gmail.com>
2023-03-10 17:15:20 +0900

    vote system(one-way) added 	modified:   app/models.py 	modified:   app/schemas.py 	modified:   app/database.py 	modified:   app/main.py 	modified:   app/models.py 	modified:   app/oauth2.py 	modified:   app/routers/post.py 	modified:   app/schemas.py 	app/config.py 	app/routers/vote.py

--- app/models.py
+++ app/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, Boolean
+from sqlalchemy import Column, ForeignKey, Integer, String, Boolean
 from sqlalchemy.sql.expression import text
 from sqlalchemy.sql.sqltypes import TIMESTAMP
 
@@ -13,7 +13,7 @@ class Post(Base):
     content = Column(String, nullable=False)
     published = Column(Boolean, server_default='TRUE', nullable=False)
     created_at = Column(TIMESTAMP(timezone=True), nullable=False, server_default=text('NOW()'))
-
+    owner_id = Column(Integer, ForeignKey("users.id", ondelete="CASCADE"), nullable=False)
 
 class User(Base):
     __tablename__ = "users"
